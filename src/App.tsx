cd352a4a7c57e2e837da249493f64299b4ef995a
KhoiNguyen <thorwaitson@gmail.com>
2022-12-03 23:05:35 -0600

    update folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import PDFViewer from "./components/PDFViewer";
 import pdf from "./assets/resume.pdf";
 
-export default <PDFViewer pdfURL={pdf} />;
+const App = () => <PDFViewer pdfURL={pdf} />;
+
+export default App;
