616be5b4b510b9f6ae292b3968a41d666bfcd1df
KhoiUna <thorwaitson@gmail.com>
2023-05-21 00:17:07 -0500

    modified:   tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   important: "#tailwind-pdf-viewer",
 };
